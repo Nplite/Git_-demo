98907d89f82720cb398bc78fbc986bd390e544a7
Nplite11 <namdeopatil.1995@gmail.com>
2023-11-25 16:04:24 +0530

    updated

--- project/alpha.py
+++ project/alpha.py
@@ -1,2 +1,3 @@
 "this is the best tool for leasrning the github"
 "My name is Namdeo Patil and I'm working as an AI and ML developer"
+'recently I have created the special form of the table and thats the table will gives the best quiality of the code'
